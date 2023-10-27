6f1f360df351bb34e6c832023601b79d3edff301
Hycarus <mirkomarasco08@gmail.com>
2023-10-27 15:04:23 +0200

    added background image

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ createApp({
         return {
             text: 'Buonanotte stelline!',
             image: "../img/stars.jpeg",
+            backgroundImage: "background-image: url(../img/stars.jpeg); background-repeat: no-repeat; background-size: cover; background-position: center;",
+            textColor: "text-light",
         }
     },
     // contiene le funzioni e i metodi
